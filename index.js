44e50e031d1ea6106ddf4689490b26b6f557d530
Mikhail Arikainen <me@aptma.one>
2023-12-11 19:57:27 +0300

    Add functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+export const concatStrings = (str1, str2) => `${str1}${str2}`
 
+export const isString = (arg) => typeof arg === 'string'
+
+export const identifySign = (num) => {
+    if (num > 0) {
+        return 'Положительное число'
+    } else if (num < 0) {
+        return 'Отрицательное число'
+    } else {
+        return 'Ноль'
+    }
+}
+
+export const reverseWords = (str) => {
+    const buf = str.split(' ')
+    let revbuf = []
+    for (let i = buf.length - 1; i >= 0; i--) {
+        revbuf.push(buf[i])
+    }
+    return revbuf.join(" ")
+}
+
+export const wordsCount = (str) => {
+    if (str.length === 0) return 0
+    const buf = str.split(" ")
+    return buf.length
+}
